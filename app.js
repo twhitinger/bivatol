ffa5f3ecfe43bda58a8407b4a9a942a811cc8f16
Trent Whitinger <twhitinger@gmail.com>
2016-02-20 18:36:58 -0600

    Adding error messages
    Adding error messages

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 //Problem: We need a simple way to look at a user's badge count and JavaScript points
 //Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 var https = require("https");
-var username = "trentwhitinger";
+var http = require('http');
+var username = "trentwhitinger123";
+
+//Print out message
+
 
 function printMessage(username, badgeCount, points) {
   var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
   console.log(message);
 }
+//Print out error messages
+function printError(error) {
+console.error(error.message);
+}
+
 
 //Connect to the API URL (http://teamtreehouse.com/username.json)
 var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response){
@@ -16,16 +25,26 @@ var request = https.get("https://teamtreehouse.com/" + username + ".json", funct
     body += chunk;
   });
   response.on('end', function() {
+    if(response.statusCode === 200){
+    try {
     var profile = JSON.parse(body);
     printMessage(username, profile.badges.length, profile.points.JavaScript);
+    } catch(error){
+      //Parse Error
+      printError(error);
+    }
+  } else {
+    //Status Code Error
+    printError({message: "There was an error getting the profile for " + username + ". (" + http.STATUS_CODES[response.statusCode] + ")"});
+  
+  }
     
-    
-   
   });
+    
   //Parse the data
   //Print the data
 });
 
-request.on("error", function(error){
-  console.error(error.message);
-});
+//Connection Error
+request.on("error", printError);
+
